7d594cdedee2cbe90718c041f38a3147d2d843cd
Katy McCann <katy.alice.mccann@gmail.com>
2016-11-29 11:43:14 +0000

    initialize deposit and debit in account at zero with attr_reader

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,11 @@
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :debit, :deposit
 
   def initialize
     @balance = 0
+    @debit = 0
+    @deposit = 0
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,4 +4,12 @@ describe Account do
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
+
+  it 'has a debit of zero' do
+    expect(subject.debit).to eq(0)
+  end
+
+  it 'has a deposit of zero' do
+    expect(subject.debit).to eq(0)
+  end 
 end
