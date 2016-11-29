05581d58e766570cc3a7be35dcbd7697b0d0791f
Katy McCann <katy.alice.mccann@gmail.com>
2016-11-29 13:10:35 +0000

    Add empty array to Date_of_transaction

--- lib/date_of_transaction.rb
+++ lib/date_of_transaction.rb
@@ -1,4 +1,10 @@
 
 class Date_of_transaction
 
+  attr_reader :statment
+
+  def initialize
+    @statment = []
+  end
+
 end
--- spec/date_of_transaction_spec.rb
+++ spec/date_of_transaction_spec.rb
@@ -1,6 +1,9 @@
 require 'date_of_transaction'
 
 describe Date_of_transaction do
+    it "has an array" do
+      dates = Date_of_transaction.new
 
-
+       expect(dates.statment).to match_array([])
+    end
 end
