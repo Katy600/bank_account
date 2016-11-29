eb1a5671d33898fc37955e0a89405e77e750b77e
Katy McCann <katy.alice.mccann@gmail.com>
2016-11-29 12:17:56 +0000

    Create a method deposit that allows you to add money to your balance and debit record

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,16 @@
 class Account
 
-  attr_reader :balance, :debit, :deposit
+  attr_reader :balance, :debit, :credit
 
   def initialize
     @balance = 0
     @debit = 0
-    @deposit = 0
+    @credit = 0
+  end
+
+  def deposit(amount)
+    @balance += amount
+    @debit += amount
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,7 +9,19 @@ describe Account do
     expect(subject.debit).to eq(0)
   end
 
-  it 'has a deposit of zero' do
-    expect(subject.debit).to eq(0)
-  end 
+  it 'has a credit of zero' do
+    expect(subject.credit).to eq(0)
+  end
+
+  it 'can deposit money into the account' do
+    account = Account.new
+    account.deposit(1000)
+    expect(account.balance).to eq(1000)
+  end
+
+  it 'can deposit money into the account so that you can view the amount that has been debited' do
+    account = Account.new
+    account.deposit(1000)
+    expect(account.debit).to eq(1000)
+  end
 end
